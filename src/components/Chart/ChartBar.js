381959e1d2c48a5444dc483abc06389067c67af6
Prabhat Dutt <prabhatdutt95@gmail.com>
2022-06-03 01:20:45 +0530

    Style Issue Fix

--- src/components/Chart/ChartBar.js
+++ src/components/Chart/ChartBar.js
@@ -7,7 +7,7 @@ const ChartBar = (props) => {
   }
   return (
     <div className="chart-bar">
-      <div className="chart-bar__innner">
+      <div className="chart-bar__inner">
         <div
           className="chart-bar__fill"
           style={{ height: barFillHeight }}
