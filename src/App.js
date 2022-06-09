1f0f7db0311378e321fc5822bffa1ad5de8b98f7
Prabhat Dutt <prabhatdutt95@gmail.com>
2022-06-09 11:09:09 +0530

    Initial Code

--- src/App.js
+++ src/App.js
@@ -1,59 +1,26 @@
-import { useState } from "react";
-import Expenses from "./components/Expenses/Expenses";
-import NewExpense from "./components/NewExpense/NewExpense";
+import React, { useState } from 'react';
 
-const DUMMY_EXPENSES = [
-  {
-    id: "e1",
-    title: "Toilet Paper",
-    amount: 94.12,
-    date: new Date(2020, 7, 14),
-  },
-  {
-    id: "e2",
-    title: "New Television",
-    amount: 799.49,
-    date: new Date(2021, 2, 12),
-  },
-  {
-    id: "e3",
-    title: "Car Insurance",
-    amount: 294.67,
-    date: new Date(2021, 2, 28),
-  },
-  {
-    id: "e4",
-    title: "New Desk (Wooden)",
-    amount: 450,
-    date: new Date(2021, 5, 12),
-  },
-  {
-    id: "e5",
-    title: "Restaurant",
-    amount: 317,
-    date: new Date(2022, 5, 2),
-  },
-];
+import AddUser from './components/Users/AddUser';
+import UsersList from './components/Users/UsersList';
 
-const App = () => {
-  const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
+function App() {
+  const [usersList, setUsersList] = useState([]);
 
-  const expenseDataChangeHandler = (expense) => {
-    setExpenses((previousExpenses) => {
-      return [expense, ...previousExpenses];
+  const addUserHandler = (uName, uAge) => {
+    setUsersList((prevUsersList) => {
+      return [
+        ...prevUsersList,
+        { name: uName, age: uAge, id: Math.random().toString() },
+      ];
     });
-  };
-
-  const saveDataHandler = (enteredExpenseData) => {
-    expenseDataChangeHandler(enteredExpenseData);
   };
 
   return (
     <div>
-      <NewExpense onAddExpense={saveDataHandler} />
-      <Expenses expenses={expenses} />
+      <AddUser onAddUser={addUserHandler} />
+      <UsersList users={usersList} />
     </div>
   );
-};
+}
 
 export default App;
