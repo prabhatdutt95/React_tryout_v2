064cba33af8fb5124ed2a2e889420efedaca4000
Prabhat Dutt <prabhatdutt95@gmail.com>
2022-06-14 09:36:54 +0530

    Managing Cart & Modal State

---
+++ src/components/Cart/Cart.js
@@ -0,0 +1,29 @@
+import Modal from "../UI/Modal";
+import classes from "./Cart.module.css";
+
+const Cart = (props) => {
+  const cartItems = (
+    <ul className={classes["cart-items"]}>
+      {[{ id: "c1", name: "Sushi", amount: 2, price: 29.99 }].map(
+        (item) => item.name
+      )}
+    </ul>
+  );
+  return (
+    <Modal onClose={props.onClose}>
+      {cartItems}
+      <div className={classes.total}>
+        <span>Total Amount</span>
+        <span>29.99</span>
+      </div>
+      <div className={classes.actions}>
+        <button className={classes["button--alt"]} onClick={props.onClose}>
+          Close
+        </button>
+        <button className={classes.button}>Order</button>
+      </div>
+    </Modal>
+  );
+};
+
+export default Cart;
